355d5a257ac21b6c582d1955e353eaaf7a36ca16
LeandroFMendes <77135135+LeandroFMendes@users.noreply.github.com>
2021-04-02 02:56:42 -0300

    Add files via upload

---
+++ aluraflix/dist/script.js
@@ -0,0 +1,7 @@
+//var filmes = ["O Senhor dos Anéis - A sociedade do Anel", "O Senhor dos Anéis - As Duas Torres", "O Senhor dos Anéis - O Retorno do Rei"]
+
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for(var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">")
+}
